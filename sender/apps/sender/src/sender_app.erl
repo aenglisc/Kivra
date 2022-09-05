cf7fc05d9c69573f3f0592b83fee862a6e291895
Roman Pushkov <pushkovroman@me.com>
2022-09-05 13:45:25 +0200

    Add db deps

--- sender/apps/sender/src/sender_app.erl
+++ sender/apps/sender/src/sender_app.erl
@@ -10,6 +10,14 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    {ok, C} = epgsql:connect(#{
+        host => "db",
+        username => "postgres",
+        password => "postgres",
+        database => "postgres",
+        timeout => 4000
+    }),
+    pgsql_migration:migrate(C, "priv/migrations"),
     sender_sup:start_link().
 
 stop(_State) ->
