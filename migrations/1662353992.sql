a97c31cbf0a01291c9c4f61ad901de827bb8914b
Roman Pushkov <pushkovroman@me.com>
2022-09-05 17:20:30 +0200

    Rework initial migration

--- migrations/1662353992.sql
+++ migrations/1662353992.sql
@@ -1,21 +1,31 @@
+-- Create content table
 -- :up
+CREATE TYPE PAYMENT_STATUS AS ENUM
+    ('unpaid', 'pending', 'paid');
+
 CREATE TABLE content(
   id serial NOT NULL,
-  payable boolean NOT NULL,
   sender_id character varying(255) NOT NULL,
   receiver_id character varying(255) NOT NULL,
   file_type character varying(255) NOT NULL,
-  mds character varying(255) NOT NULL,
-  data bytea NOT NULL
-
-  UNIQUE (sender_id, receiver_id, file_type, md5)
+  file_name character varying(255) NOT NULL,
+  is_payable boolean NOT NULL DEFAULT FALSE,
+  payment_status PAYMENT_STATUS NOT NULL DEFAULT 'unpaid',
+  file_content bytea NOT NULL,
+  md5 bytea NOT NULL,
   CONSTRAINT content_pkey PRIMARY KEY (id)
 );
 
+-- create index
 CREATE INDEX index_content_on_receiver_id
-  ON 
+  ON content
   USING btree
-  (receiver_id);
+  (receiver_id COLLATE pg_catalog."default");
 
+CREATE UNIQUE INDEX unique_content
+  ON content
+  (sender_id, receiver_id, md5);
+
+-- Drop content in downgrade
 -- :down
 DROP TABLE content;
