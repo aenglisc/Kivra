b17ac3ec590ce6d0f9ac083b3ca9a7127c40e905
Roman Pushkov <pushkovroman@me.com>
2022-09-06 07:28:40 +0200

    Add payment implementation

--- consumer/apps/consumer/src/consumer_logic_handler.erl
+++ consumer/apps/consumer/src/consumer_logic_handler.erl
@@ -13,6 +13,16 @@
 handle_request('ReceiverIdContentGet', _Req, #{receiver_id := ReceiverID}) ->
     Storage = storage(),
     {200, #{}, Storage:list_content(ReceiverID)};
+handle_request('ContentPayPatch', _Req, Context) ->
+    Body = maps:get('Payment', Context),
+    ContentID = maps:get(<<"content_id">>, Body),
+    Storage = storage(),
+    case Storage:pay_for_content(ContentID) of
+        {ok, success} -> {201, #{}, #{message => <<"Success">>}};
+        {error, paid} -> {200, #{}, #{message => <<"Already paid">>}};
+        {error, not_payable} -> {422, #{}, #{message => <<"Not payable">>}};
+        {error, not_found} -> {404, #{}, #{message => <<"Not found">>}}
+    end;
 handle_request(OperationID, Req, Context) ->
     error_logger:error_msg("Unknown request to process: ~p~n", [{OperationID, Req, Context}]),
     {501, #{}, #{}}.
--- consumer/apps/consumer/src/consumer_storage.erl
+++ consumer/apps/consumer/src/consumer_storage.erl
@@ -1,6 +1,6 @@
 -module(consumer_storage).
 
--export([start/0, list_content/1, update_payment_status/1]).
+-export([start/0, list_content/1, pay_for_content/1]).
 
 -type receiver_id() :: binary().
 -type content_id() :: binary().
@@ -18,6 +18,7 @@
 -callback start() -> ok.
 -callback list_content(receiver_id()) -> [content()].
 -callback update_payment_status(content_id()) -> ok.
+-callback pay_for_content(content_id()) -> {ok, success} | {error, not_found | not_payable | paid}.
 
 start() ->
     {ok, Conn} = epgsql:connect(#{
@@ -39,33 +40,69 @@ start() ->
     ok.
 
 list_content(ReceiverID) ->
+    Keys = [
+        content_id,
+        file_content,
+        file_name,
+        file_type,
+        sender_id,
+        receiver_id,
+        payment_status,
+        is_payable
+    ],
     Query = lists:concat([
         "select id, file_content, file_name, file_type, sender_id, receiver_id, payment_status, is_payable from content ",
         "where receiver_id = $1 "
     ]),
     case pgapp:with_transaction(fun() -> pgapp:equery(Query, [ReceiverID]) end) of
-        {ok, _Column, Results} ->
-            Keys = [
-                content_id,
-                file_content,
-                file_name,
-                file_type,
-                sender_id,
-                receiver_id,
-                payment_status,
-                is_payable
-            ],
-            Zip = f:curry(fun lists:zip/2),
-            ZipWithKeys = Zip(Keys),
-            MapperPipeline = [
-                fun tuple_to_list/1,
-                ZipWithKeys,
-                fun maps:from_list/1
-            ],
-            ResultMapper = (f:flip(fun f:pipe/2))(MapperPipeline),
-            lists:map(ResultMapper, Results)
+        {ok, _Columns, Results} ->
+            lists:map(result_mapper(Keys), Results)
     end.
 
-update_payment_status(_ContentID) ->
-    erlang:display(implementing),
-    ok.
+pay_for_content(ContentID) ->
+    f:do(ContentID, [
+        fun get_payment_data/1,
+        fun validate_payability/1,
+        fun validate_status/1,
+        fun update_payment_status/1
+    ]).
+
+get_payment_data(ContentID) ->
+    Keys = [
+        content_id,
+        payment_status,
+        is_payable
+    ],
+    Query = lists:concat([
+        "select id, payment_status, is_payable from content ",
+        "where id = $1 "
+    ]),
+    case pgapp:with_transaction(fun() -> pgapp:equery(Query, [ContentID]) end) of
+        {ok, _Columns, []} -> f:err(not_found);
+        {ok, _Columns, [Result]} -> f:ok((result_mapper(Keys))(Result))
+    end.
+
+validate_payability(#{is_payable := true} = PaymentData) -> f:ok(PaymentData);
+validate_payability(#{is_payable := false}) -> f:err(not_payable).
+
+validate_status(#{payment_status := <<"unpaid">>} = PaymentData) -> f:ok(PaymentData);
+validate_status(#{payment_status := <<"paid">>}) -> f:err(paid).
+
+update_payment_status(#{content_id := ContentID}) ->
+    Query = lists:concat([
+        "update content ",
+        "set payment_status = 'paid' ",
+        "where id = $1 "
+    ]),
+    case pgapp:with_transaction(fun() -> pgapp:equery(Query, [ContentID]) end) of
+        {ok, 1} -> {ok, success}
+    end.
+
+result_mapper(Keys) ->
+    Zip = f:curry(fun lists:zip/2),
+    ZipWithKeys = Zip(Keys),
+    (f:flip(fun f:pipe/2))([
+        fun tuple_to_list/1,
+        ZipWithKeys,
+        fun maps:from_list/1
+    ]).
